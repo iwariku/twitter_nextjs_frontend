4bd1c34e8a92d80c0b0a68093e0b5ee4c34aa9f0
iwariku <02rikuto18@gmail.com>
2026-04-10 17:11:31 +0900

    feat: SignUpとLogin時のリクエスト内容を作成、Login時のCookieにsession_idを設定する機能の実装

--- features/auth/actions/actions.ts
+++ features/auth/actions/actions.ts
@@ -1,15 +1,64 @@
 'use server';
 
+import { cookies } from 'next/headers';
+import { parse } from 'cookie';
 import { redirect } from 'next/navigation';
 
 export const SignUp = async (formData: FormData) => {
   const email = formData.get('email');
   const password = formData.get('password');
+
+  const response = await fetch('http://localhost:8080/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    console.error('新規登録に失敗しました');
+    redirect('/signup');
+  }
+
   redirect('/login');
 };
 
 export const Login = async (formData: FormData) => {
   const email = formData.get('email');
   const password = formData.get('password');
+
+  const response = await fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    console.error('ログインに失敗しました。');
+    redirect('/login');
+  }
+
+  const cookieHeader = response.headers.get('set-cookie');
+  if (cookieHeader) {
+    await SetSessionIdCookie(cookieHeader);
+  }
+
   redirect('/home');
+};
+
+export const SetSessionIdCookie = async (cookieHeader: string) => {
+  const parsedCookies = parse(cookieHeader);
+  const sessionId = parsedCookies.session_id;
+
+  if (sessionId) {
+    const cookieStore = await cookies();
+    cookieStore.set('session_id', sessionId, {
+      httpOnly: true,
+      maxAge: 86400,
+      path: '/',
+    });
+  }
 };
