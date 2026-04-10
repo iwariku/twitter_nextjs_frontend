203a0877400784d8a00d11eb7e16b289eabe4667
iwariku <02rikuto18@gmail.com>
2026-04-10 18:33:13 +0900

    refactor: 認証ガードをauthGroup()からmiddleware.tsでの一括管理に変更

--- features/auth/actions/actions.ts
+++ features/auth/actions/actions.ts
@@ -67,11 +67,3 @@ export const getSessionId = async () => {
   const cookieStore = await cookies();
   return cookieStore.get('session_id')?.value;
 };
-
-export const authGuard = async () => {
-  const sessionId = await getSessionId();
-  if (!sessionId) {
-    redirect('/login');
-    return sessionId;
-  }
-};
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { authGuard } from '../../../features/auth/actions/actions';
 
 const HomePage = async () => {
-  await authGuard();
-
   return (
     <div>
       <h1>ホーム画面</h1>
