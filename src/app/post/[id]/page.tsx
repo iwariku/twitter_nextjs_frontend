56220831b5ece5bc02b575fb37706f07215e9d4b
iwariku <02rikuto18@gmail.com>
2026-04-15 13:30:02 +0900

    feat: パスからidを取得し、取得したidのツイートを画面に表示する機能の実装

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { getSessionId } from '@/features/auth/actions/actions';
 
-const page = () => {
+const TweetDetail = async ({ params }) => {
+  const sessionId = await getSessionId();
+  const { id } = await params;
+
+  // ここを別で定義する場合は、getTweetという関数名にすればいい
+  const response = await fetch(`${process.env.API_BASE_URL}/api/tweets/${id}`, {
+    method: 'GET',
+    headers: {
+      Cookie: `session_id=${sessionId}`,
+    },
+  });
+
+  const tweet = await response.json();
+
   return (
     <div>
-      <h1>テスト1</h1>
+      <h1>ツイート詳細画面</h1>
+      <h1>{tweet.content}</h1>
     </div>
   );
 };
 
-export default page;
+export default TweetDetail;
