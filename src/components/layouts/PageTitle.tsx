52449db5183c844a5aa3da98a4dc170fcb1c57ba
iwariku <02rikuto18@gmail.com>
2026-04-20 23:24:33 +0900

    feat: ユーザーIDからユーザー詳細へのページ遷移を実装

--- src/components/layouts/PageTitle.tsx
+++ src/components/layouts/PageTitle.tsx
@@ -4,7 +4,7 @@ type PropsType = {
 
 const PageTitle = ({ title }: PropsType) => {
   return (
-    <div className="sticky top-0 bg-white/80 backdrop-blur-sm p-4 border-b border-gray-100 z-10">
+    <div className="sticky top-0 bg-white/80 backdrop-blur-sm p-4 border-b border-gray-100  z-30">
       <h2 className="text-xl font-bold">{title}</h2>
     </div>
   );
