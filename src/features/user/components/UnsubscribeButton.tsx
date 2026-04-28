dff39da5af578245bf159eab507472acccb799ef
iwariku <02rikuto18@gmail.com>
2026-04-28 15:41:52 +0900

    refactor: UnsubscribeButton.tsxの削除

--- src/features/user/components/UnsubscribeButton.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { unsubscribe } from '../actions/unsubscribe';
-
-const UnsubscribeButton = () => {
-  return <button onClick={unsubscribe}>退会する</button>;
-};
-
-export default UnsubscribeButton;
