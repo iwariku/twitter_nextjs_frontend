06f4ce5b94da0f0fecb660382485fa75805530fc
iwariku <02rikuto18@gmail.com>
2026-06-13 00:48:05 +0900

    ci: yamlファイルにCIを記述

---
+++ dangerfile.js
@@ -0,0 +1,14 @@
+// 追加行と削除行の合計が200行を超えた場合;
+const totalChanges = danger.github.pr.changes;
+if (totalChanges > 200) {
+  warn('diffが200行を超えています');
+}
+
+// 編集ファイル数が10ファイルを超えた場合;
+const modifiedFilesCount = danger.github.pr.modified_files.length;
+const createdFilesCount = danger.github.pr.created_files.length;
+const totalFiles = modifiedFilesCount + createdFilesCount;
+
+if (totalFiles > 10) {
+  warn('編集ファイル数が10ファイル超えています');
+}
